d8af0ead3ac7ba5d4bff0444ec53515b3bb2a79c
valentine-mario <valentine13400@gmail.com>
2020-01-20 13:38:48 +0100

    setup feature module and lazy loading

---
+++ src/app/user/user-routing-module.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+export const UserRoutes: Routes = [
+    {
+        path:'',
+        component:HomeComponent,
+        
+    }
+]
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {UserRoutes} from './user-routing-module';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(UserRoutes)
   ]
 })
 export class UserModule { }
