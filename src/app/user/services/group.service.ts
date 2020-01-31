227da0c0c3da75b5550396872222ee9628cedce2
valentine-mario <valentine13400@gmail.com>
2020-01-31 13:38:51 +0100

    added services for group

--- src/app/user/services/group.service.ts
+++ src/app/user/services/group.service.ts
@@ -7,5 +7,53 @@ import {HttpHeaders} from '@angular/common/http';
 })
 export class GroupService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  addGroup(data){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+`/employee/add`, data, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  editGroup(data, id){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+`/employee/edit/${id}`, data, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  deleteGroup(id){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/employee/delete/${id}`, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  getGroups(page, limit){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/employee/get?page=${page}&limit=${limit}`, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  getOneGroup(id){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/employee/getone/${id}`, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  deleteNameFromGroup(id, data){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+`/employee/deletename/${id}`, data, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
 }
