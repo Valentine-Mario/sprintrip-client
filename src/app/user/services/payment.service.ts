01ab07b78e2c066d426f1c05e99c9735058c3f96
valentine-mario <valentine13400@gmail.com>
2020-01-31 13:44:21 +0100

    added services for cards

--- src/app/user/services/payment.service.ts
+++ src/app/user/services/payment.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-
+import {AppEndpoint} from '../endpoint'
+import { HttpClient } from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  addCard(data){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+`/card/add`, data, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  getCards(){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/card/get`, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  deleteCards(id){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/card/delete/${id}`, {  
+      observe: 'response',
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
 }
