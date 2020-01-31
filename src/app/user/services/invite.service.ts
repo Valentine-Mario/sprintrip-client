8cbcd6fbc83b9351b1d202ba1c5bb6c642c7ff69
valentine-mario <valentine13400@gmail.com>
2020-01-31 15:29:44 +0100

    added invite user services

--- src/app/user/services/invite.service.ts
+++ src/app/user/services/invite.service.ts
@@ -1,9 +1,66 @@
 import { Injectable } from '@angular/core';
+import {AppEndpoint} from '../endpoint'
+import { HttpClient } from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InviteService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  deleteInvitedUser(id){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+'/invite/delete/'+id, {
+     observe: 'response',
+     headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': "bearer "+authToken}),
+   })
+  }
+
+  getAllInvitedUsers(){
+    let authToken= localStorage.getItem('invite-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+'/invite/get', {
+     observe: 'response',
+     headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': "bearer "+authToken}),
+   })
+  }
+
+  InvitedUserEdit(data){
+    let authToken= localStorage.getItem('invite-token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+'/invite/edit', data, {
+     observe: 'response',
+     headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': "bearer "+authToken}),
+   })
+  }
+
+  InvitedUserEditPassword(data){
+    let authToken= localStorage.getItem('invite-token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+'/invite/changepassword', data, {
+     observe: 'response',
+     headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': "bearer "+authToken}),
+   })
+  }
+
+  editInvitedUserccess(data, id){
+    let authToken= localStorage.getItem('user-token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+'/invite/editsetting/'+id, data, {
+     observe: 'response',
+     headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': "bearer "+authToken}),
+   })
+  }
+
+  getInvitedUserById(id){
+    return this.http.get(AppEndpoint.API_ENDPOINT+'/invite/getone/'+id, {
+      observe:'response'
+    })
+  }
+
+  ApproveOrDeclineBooking(data, id){
+    let authToken= localStorage.getItem('invite-token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+'/approval/approvedisapprove/'+id, data, {
+     observe: 'response',
+     headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': "bearer "+authToken}),
+   })
+  }
 }
